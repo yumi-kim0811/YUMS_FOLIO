ff2ca55e8c34ddf9f90135597d8c0e7c4a3627ca
project <gowl91@naver.com>
2020-06-05 22:23:04 +0900

    .

---
+++ js/index-menu.js
@@ -0,0 +1,26 @@
+$(function(){
+   //start
+    
+    //별 세개 누르면 메뉴 오픈 
+    $('.menu a').click(function(){
+        $('#menu-div').css({
+            top:0
+        });
+    });
+    
+    //엑스버튼 누르면 메뉴 닫힘
+    $('#menu-div button').click(function(){
+       $('#menu-div').css({
+           top:'-100vh'
+       });
+    });
+    
+    $('#menu-div nav a').eq(0).click(function(e){
+        e.preventDefault();
+       $('#menu-div').css({
+           top:'-100vh'
+       });
+    });
+    
+    //end
+});
